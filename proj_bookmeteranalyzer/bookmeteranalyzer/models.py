80a65831984bfb10e5bafeeb68567cdaeda449ea
natsuyasai <natsuyasai7777@gmail.com>
2018-09-29 00:35:43 +0900

    csvダウンロード追加

--- proj_bookmeteranalyzer/bookmeteranalyzer/models.py
+++ proj_bookmeteranalyzer/bookmeteranalyzer/models.py
@@ -10,5 +10,6 @@ class AnalyzeResult(models.Model):
     # ユーザIDを主キーとし，画像ファイルパスを保持する
     user_id = models.CharField(primary_key=True, max_length=20)
     img_file = models.ImageField(upload_to='image/')
+    csv_file = models.FileField(upload_to='csv/')
 
 
--- proj_bookmeteranalyzer/bookmeteranalyzer/views.py
+++ proj_bookmeteranalyzer/bookmeteranalyzer/views.py
@@ -16,12 +16,16 @@ from .script.analyzer import execAnalyze
 
 # Create your views here.
 def index(request):
+    """ デフォルトページ表示
+    """
     posts = Post.objects.all()
     form = PostForm()
     context = {'posts': posts, 'form': form, }
     return render(request, 'bookmeteranalyzer/index.html', context)
     
 def analyze(request):
+    """ 解析処理実行
+    """
     form = PostForm(request.POST)
     analyze_user_id = request.POST.get('user_id')
     # 文字列が数値でなければ処理終了
@@ -35,19 +39,27 @@ def analyze(request):
     execAnalyze(analyze_user_id)
     # 結果の画像を登録
     img_filename = analyze_user_id + '.png'
-    #csv_filename = analyze_user_id + '.csv'
     img_open = open('./bookmeteranalyzer/analyzedata/image/' + img_filename, 'rb')
+    # 解析用csv登録
+    csv_filename = analyze_user_id + '.csv'
+    csv_open = open('./bookmeteranalyzer/analyzedata/csv/' + csv_filename, 'rb')
     analyze_rslt = AnalyzeResult()
     analyze_rslt.user_id = analyze_user_id
     analyze_rslt.img_file.save(img_filename, File(img_open), save=True)
+    analyze_rslt.csv_file.save(csv_filename, File(csv_open), save=True)
     # 結果生成
     # 指定ユーザIDの画像をDBから取得
-    img_file = AnalyzeResult.objects.filter(user_id=analyze_user_id).first()
-    # ImgaeFiledをjson化
-    to_json = json.dumps(str(img_file.img_file))
-    rslt_str = "media/" + to_json.replace('"','')
-    print(rslt_str)
-    return JsonResponse({'img_file_url': rslt_str})
+    user_data = AnalyzeResult.objects.filter(user_id=analyze_user_id).first()
+    # ImgaeFiledとFileFildをjson化
+    img_file_to_json = json.dumps(str(user_data.img_file))
+    csv_file_to_json = json.dumps(str(user_data.csv_file))
+    # URL生成
+    img_url_str = "media/" + img_file_to_json.replace('"','')
+    csv_url_str = "media/" + csv_file_to_json.replace('"','')
+    print(img_url_str)
+    print(csv_url_str)
+    # 結果をjsonとして返す
+    return JsonResponse({'img_file_url' : img_url_str, 'csv_file_url' : csv_url_str})
     """
     return render(
         request,
