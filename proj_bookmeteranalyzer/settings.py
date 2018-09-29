74a33e4a6f04ae7d53447ae50cf020c342942a6e
natsuyasai <natsuyasai7777@gmail.com>
2018-09-29 19:50:03 +0900

    s3バケット名設定

--- proj_bookmeteranalyzer/settings.py
+++ proj_bookmeteranalyzer/settings.py
@@ -139,7 +139,7 @@ ALLOWED_HOSTS = ['*']
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_ACCESS_KEY_ID = ''
 AWS_SECRET_ACCESS_KEY = ''
-AWS_STORAGE_BUCKET_NAME = ''
+AWS_STORAGE_BUCKET_NAME = 'bookmeteranalyzer-media'
 
 DEBUG = False
 
